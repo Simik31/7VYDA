86f9dd4674a17cad60402bf5e6158fbe979aaf40
Simik31 <40343348+Simik31@users.noreply.github.com>
2020-10-10 01:01:17 +0200

    Add files via upload

--- scripts/Logout.php
+++ scripts/Logout.php
@@ -6,9 +6,6 @@ $_SESSION["logged"] = "";
 $_SESSION["ID"] = "";
 $_SESSION["role"] = "";
 $_SESSION["Name"] = "";
-$_SESSION["index"] = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'];
-if($_SERVER['HTTP_HOST'] === 'localhost') {
-    $_SESSION["index"] .= "/Seminarka/";
-}
+$_SESSION["index"] = "";
 session_destroy();
 header("Location: ../index.php");
